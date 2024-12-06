8dc539db334203ab6b1036f7022d9bc7a2a5a581
Giovanni Hislop <giovanni.hislop@bnnvara.nl>
2024-12-06 17:49:10 +0100

    next add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
-// src/app/page.tsx
 import { useRouter } from "next/router";
-import useUser from "@/app/hooks/useUser";
 
 const HomePage = () => {
-  const user = useUser(); // Get the authenticated user
+  const user = null;
   const router = useRouter();
 
-  // Redirect to login if not logged in
   if (!user) {
-    router.push("/auth/login"); // Redirect to login page if no user is logged in
-    return <div>Redirecting...</div>; // Show loading text while redirecting
+    router.push("/auth/login");
+    return <div>Redirecting...</div>;
   }
 
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <p>Logged in as: {user.email}</p> {/* Display the user's email */}
     </div>
   );
 };
