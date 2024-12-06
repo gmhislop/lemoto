c50c203c1dc1d1463f6ca63ef87b60071f79cf66
Giovanni Hislop <giovanni.hislop@bnnvara.nl>
2024-12-06 17:44:38 +0100

    feat set up auth

---
+++ src/app/auth/login.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import supabase from "@/app/lib/supabaseClient";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [user, setUser] = useState<any>(null);
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      setUser(data?.user);
+      router.push("/");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {user && <p>Welcome back, {user.email}!</p>}{" "}
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
